fe23643e02abc7ce74cdf267523e28204438426d
dinitrogen <noshowdon@gmail.com>
2021-06-27 21:37:27 -0400

    Added edit player functionality and organized roster code.

--- src/roster.js
+++ src/roster.js
@@ -22,7 +22,7 @@ function calculateOBP(player) {
 }
 
 
-//at Bat prototype
+//At Bat prototype that new players inherit. But none of these methods are used so remove later.
 const atBatProto = {
     getFullName() {
         return this.firstName + " " + this.lastName;
@@ -60,7 +60,7 @@ const atBatProto = {
 }
 
 
-// Player factory function
+// Create a new player factory function
 const createPlayer = function(firstName, lastName, jerseyNum) {
     let player = Object.create(atBatProto);
     player.firstName = firstName;
@@ -84,7 +84,8 @@ const createPlayer = function(firstName, lastName, jerseyNum) {
     return player;
 }
 
-// Assign DOM elements
+// Create the DOM elements for load roster page
+
 function createNewPlayerFormDiv() {
     const newPlayerFormDiv = document.createElement('div')
     newPlayerFormDiv.setAttribute('id','newPlayerFormDiv');
@@ -101,63 +102,11 @@ function createAddPlayerButton() {
     return addPlayerButton;
 }
 
-// Create a new player form
-function createNewPlayerForm() {
-
-    if (newPlayerFormDiv.firstChild) {
-        return;
-    }
-
-    const newPlayerForm = document.createElement('form');
-
-    const newFirstNameField = document.createElement('input');
-    newFirstNameField.setAttribute('type', 'text');
-    newFirstNameField.setAttribute('placeholder', 'First name');
-    newFirstNameField.setAttribute('id', 'newFirstName');
-
-    const newLastNameField = document.createElement('input');
-    newLastNameField.setAttribute('type', 'text');
-    newLastNameField.setAttribute('placeholder', 'Last name');
-    newLastNameField.setAttribute('id', 'newLastName');
-
-    const newJerseyNumField = document.createElement('input');
-    newJerseyNumField.setAttribute('type', 'text');
-    newJerseyNumField.setAttribute('placeholder', 'Jersey number');
-    newJerseyNumField.setAttribute('id', 'newJerseyNum');
-
-    const submitButton = document.createElement('input');
-    submitButton.setAttribute('type', 'button');
-    submitButton.setAttribute('value', 'Submit');
-    submitButton.addEventListener('click', function() {
-        addPlayer();
-        displayRosterTable();
-        while (newPlayerFormDiv.firstChild) {
-            newPlayerFormDiv.removeChild(newPlayerFormDiv.lastChild);
-        }
-    });
-
-    newPlayerForm.reset();
-    newPlayerFormDiv.appendChild(newPlayerForm);
-    newPlayerForm.appendChild(newFirstNameField);
-    newPlayerForm.appendChild(newLastNameField);
-    newPlayerForm.appendChild(newJerseyNumField);
-    newPlayerForm.appendChild(submitButton);
-}
-
-function addPlayer() {
-    let newFirstName = document.getElementById('newFirstName').value;
-    let newLastName = document.getElementById('newLastName').value;
-    let newJerseyNum = document.getElementById('newJerseyNum').value;
-    let newPlayer = createPlayer(newFirstName, newLastName, newJerseyNum);
-    myRoster.push(newPlayer);
-    saveRoster();
-    console.table(myRoster);
-}
-
 function createRosterTable() {
     const rosterTable = document.createElement('table');
     const headerRow = document.createElement('tr');
     
+    // Player stats headers
     const nameHeader = document.createElement('th');
     nameHeader.textContent = 'Name';
     headerRow.appendChild(nameHeader);
@@ -218,20 +167,24 @@ function createRosterTable() {
     obpHeader.textContent = 'OBP';
     headerRow.appendChild(obpHeader);
 
-    // other headers
-
+    // Buttons headers
     const addRunHeader = document.createElement('th');
     addRunHeader.textContent = 'Add run';
     headerRow.appendChild(addRunHeader);
 
     const moveHeader = document.createElement('th');
-    moveHeader.textContent = 'Order';
+    moveHeader.textContent = 'Change order';
     headerRow.appendChild(moveHeader);
+
+    const editHeader = document.createElement('th');
+    editHeader.textContent = 'Edit player';
+    headerRow.appendChild(editHeader);
 
     const delHeader = document.createElement('th');
     delHeader.textContent = 'Remove';
     headerRow.appendChild(delHeader);
-    
+
+    // Add header row to table
     rosterTable.appendChild(headerRow);
     const tbody = document.createElement('tbody');
     tbody.setAttribute('id','tbody');
@@ -239,6 +192,73 @@ function createRosterTable() {
 
     return rosterTable;
 }
+
+
+// Create and display a new player form
+function createNewPlayerForm() {
+
+    if (newPlayerFormDiv.firstChild) {
+        return;
+    }
+
+    const newPlayerForm = document.createElement('form');
+
+    const newFirstNameLabel = document.createElement('label');
+    newFirstNameLabel.setAttribute('for','newFirstName');
+    newFirstNameLabel.textContent = 'First name:';
+    const newFirstNameField = document.createElement('input');
+    newFirstNameField.setAttribute('type', 'text');
+    newFirstNameField.setAttribute('placeholder', 'First name');
+    newFirstNameField.setAttribute('id', 'newFirstName');
+    newPlayerForm.appendChild(newFirstNameLabel);
+    newPlayerForm.appendChild(newFirstNameField);
+
+    const newLastNameLabel = document.createElement('label');
+    newLastNameLabel.setAttribute('for','newLastName');
+    newLastNameLabel.textContent = 'Last name:';
+    const newLastNameField = document.createElement('input');
+    newLastNameField.setAttribute('type', 'text');
+    newLastNameField.setAttribute('placeholder', 'Last name');
+    newLastNameField.setAttribute('id', 'newLastName');
+    newPlayerForm.appendChild(newLastNameLabel);
+    newPlayerForm.appendChild(newLastNameField);
+
+    const newJerseyNumLabel = document.createElement('label');
+    newJerseyNumLabel.setAttribute('for','newJerseyNum');
+    newJerseyNumLabel.textContent = 'Jersey #:';
+    const newJerseyNumField = document.createElement('input');
+    newJerseyNumField.setAttribute('type', 'text');
+    newJerseyNumField.setAttribute('placeholder', 'Jersey #');
+    newJerseyNumField.setAttribute('id', 'newJerseyNum');
+    newPlayerForm.appendChild(newJerseyNumLabel);
+    newPlayerForm.appendChild(newJerseyNumField);
+
+    const submitButton = document.createElement('input');
+    submitButton.setAttribute('type', 'button');
+    submitButton.setAttribute('value', 'Submit');
+    submitButton.addEventListener('click', function() {
+        addPlayer();
+        displayRosterTable();
+        while (newPlayerFormDiv.firstChild) {
+            newPlayerFormDiv.removeChild(newPlayerFormDiv.lastChild);
+        }
+    });
+
+    newPlayerForm.reset();
+    newPlayerFormDiv.appendChild(newPlayerForm);
+    newPlayerForm.appendChild(submitButton);
+}
+
+// Takes player form values to create new player and add to roster array
+function addPlayer() {
+    let newFirstName = document.getElementById('newFirstName').value;
+    let newLastName = document.getElementById('newLastName').value;
+    let newJerseyNum = document.getElementById('newJerseyNum').value;
+    let newPlayer = createPlayer(newFirstName, newLastName, newJerseyNum);
+    myRoster.push(newPlayer);
+    saveRoster();
+}
+
 
 function displayRosterTable() {
     let tbody = document.getElementById('tbody');
@@ -312,8 +332,10 @@ function displayRosterTable() {
         player.obp = calculateOBP(player);
         obpCell.textContent = `${player.obp}`;
         playerRow.appendChild(obpCell);
-        // other stats
+        
+        // Can add other stats here
 
+        // Table buttons
         const addRunCell = document.createElement('td');
         const addRunButton = document.createElement('button');
         addRunButton.textContent = 'Add run';
@@ -327,7 +349,7 @@ function displayRosterTable() {
 
         const moveCell = document.createElement('td');
         const moveButton = document.createElement('button');
-        moveButton.textContent = 'move down';
+        moveButton.textContent = 'Move down';
         moveButton.onclick = function() {
             let index = myRoster.indexOf(player);
             if (myRoster.length === 1) {
@@ -339,14 +361,22 @@ function displayRosterTable() {
             }       
             displayRosterTable();
             saveRoster();
-            console.table(myRoster); //remove later
         }
         moveCell.appendChild(moveButton);
         playerRow.appendChild(moveCell);
         
+        const editCell = document.createElement('td');
+        const editButton = document.createElement('button');
+        editButton.textContent = 'Edit player';
+        editButton.onclick = function() {
+            loadEditPlayerPage(player);
+        }
+        editCell.appendChild(editButton);
+        playerRow.appendChild(editCell);
+
         const delCell = document.createElement('td');
         const delButton = document.createElement('button');
-        delButton.textContent = 'remove player';
+        delButton.textContent = 'Remove player';
         delButton.onclick = function() {
             if (prompt("Are you sure? (type 'yes')") === 'yes') {
                 tbody.removeChild(playerRow);
@@ -362,10 +392,225 @@ function displayRosterTable() {
         
         tbody.appendChild(playerRow);
     }
+}
 
+// Player editor
+// Create edit player page DOM elements
+function createEditPlayerDiv() {
+    const editPlayerDiv = document.createElement('div');
+    editPlayerDiv.setAttribute('id', 'editPlayerDiv');
+    return editPlayerDiv;
+}
+
+function createEditPlayerForm(player) {
+    
+    const editPlayerForm = document.createElement('form');
+    editPlayerForm.setAttribute('id', 'editPlayerForm');
+    const editPlayerHeader = document.createElement('h2');
+    editPlayerHeader.textContent = `Edit player stats: ${player.firstName} ${player.lastName}`;
+    editPlayerForm.appendChild(editPlayerHeader);
+
+    const firstNameLabel = document.createElement('label');
+    firstNameLabel.setAttribute('for', 'editFirstName');
+    firstNameLabel.textContent = 'First Name:';
+    const editFirstNameField = document.createElement('input');
+    editFirstNameField.setAttribute('type', 'text');
+    editFirstNameField.setAttribute('value', `${player.firstName}`);
+    editFirstNameField.setAttribute('id', 'editFirstName');
+    editPlayerForm.appendChild(firstNameLabel);
+    editPlayerForm.appendChild(editFirstNameField);
+
+    const lastNameLabel = document.createElement('label');
+    lastNameLabel.setAttribute('for', 'editLastName');
+    lastNameLabel.textContent = 'Last Name:';
+    const editLastNameField = document.createElement('input');
+    editLastNameField.setAttribute('type', 'text');
+    editLastNameField.setAttribute('value', `${player.lastName}`);
+    editLastNameField.setAttribute('id', 'editLastName');
+    editPlayerForm.appendChild(lastNameLabel);
+    editPlayerForm.appendChild(editLastNameField);
+
+    const jerseyNumLabel = document.createElement('label');
+    jerseyNumLabel.setAttribute('for', 'editJerseyNum');
+    jerseyNumLabel.textContent = 'Jersey #:';
+    const editJerseyNumField = document.createElement('input');
+    editJerseyNumField.setAttribute('type', 'text');
+    editJerseyNumField.setAttribute('value', `${player.jerseyNum}`);
+    editJerseyNumField.setAttribute('id', 'editJerseyNum');
+    editPlayerForm.appendChild(jerseyNumLabel);
+    editPlayerForm.appendChild(editJerseyNumField);
+
+    const PALabel = document.createElement('label');
+    PALabel.setAttribute('for', 'editPA');
+    PALabel.textContent = 'PA:';
+    const editPAField = document.createElement('input');
+    editPAField.setAttribute('type', 'number');
+    editPAField.setAttribute('value', `${player.numPA}`);
+    editPAField.setAttribute('id', 'editPA');
+    editPlayerForm.appendChild(PALabel);
+    editPlayerForm.appendChild(editPAField);
+    
+    const runsLabel = document.createElement('label');
+    runsLabel.setAttribute('for', 'editRuns');
+    runsLabel.textContent = 'R:';
+    const editRunsField = document.createElement('input');
+    editRunsField.setAttribute('type', 'number');
+    editRunsField.setAttribute('value', `${player.numRuns}`);
+    editRunsField.setAttribute('id', 'editRuns');
+    editPlayerForm.appendChild(runsLabel);
+    editPlayerForm.appendChild(editRunsField);
+
+    const singlesLabel = document.createElement('label');
+    singlesLabel.setAttribute('for', 'editSingles');
+    singlesLabel.textContent = '1B:';
+    const editSinglesField = document.createElement('input');
+    editSinglesField.setAttribute('type', 'number');
+    editSinglesField.setAttribute('value', `${player.numSingles}`);
+    editSinglesField.setAttribute('id', 'editSingles');
+    editPlayerForm.appendChild(singlesLabel);
+    editPlayerForm.appendChild(editSinglesField);
+
+    const doublesLabel = document.createElement('label');
+    doublesLabel.setAttribute('for', 'editDoubles');
+    doublesLabel.textContent = '2B:';
+    const editDoublesField = document.createElement('input');
+    editDoublesField.setAttribute('type', 'number');
+    editDoublesField.setAttribute('value', `${player.numDoubles}`);
+    editDoublesField.setAttribute('id', 'editDoubles');
+    editPlayerForm.appendChild(doublesLabel);
+    editPlayerForm.appendChild(editDoublesField);
+
+    const triplesLabel = document.createElement('label');
+    triplesLabel.setAttribute('for', 'editTriples');
+    triplesLabel.textContent = '3B:';
+    const editTriplesField = document.createElement('input');
+    editTriplesField.setAttribute('type', 'number');
+    editTriplesField.setAttribute('value', `${player.numTriples}`);
+    editTriplesField.setAttribute('id', 'editTriples');
+    editPlayerForm.appendChild(triplesLabel);
+    editPlayerForm.appendChild(editTriplesField);
+
+    const homerunsLabel = document.createElement('label');
+    homerunsLabel.setAttribute('for', 'editHomeruns');
+    homerunsLabel.textContent = 'HR:';
+    const editHomerunsField = document.createElement('input');
+    editHomerunsField.setAttribute('type', 'number');
+    editHomerunsField.setAttribute('value', `${player.numHRs}`);
+    editHomerunsField.setAttribute('id', 'editHomeruns');
+    editPlayerForm.appendChild(homerunsLabel);
+    editPlayerForm.appendChild(editHomerunsField);
+
+    const walksLabel = document.createElement('label');
+    walksLabel.setAttribute('for', 'editWalks');
+    walksLabel.textContent = 'BB:';
+    const editWalksField = document.createElement('input');
+    editWalksField.setAttribute('type', 'number');
+    editWalksField.setAttribute('value', `${player.numWalks}`);
+    editWalksField.setAttribute('id', 'editWalks');
+    editPlayerForm.appendChild(walksLabel);
+    editPlayerForm.appendChild(editWalksField);
+    
+    const strikeoutsLabel = document.createElement('label');
+    strikeoutsLabel.setAttribute('for', 'editStrikeouts');
+    strikeoutsLabel.textContent = 'SO:';
+    const editStrikeoutsField = document.createElement('input');
+    editStrikeoutsField.setAttribute('type', 'number');
+    editStrikeoutsField.setAttribute('value', `${player.numStrikeouts}`);
+    editStrikeoutsField.setAttribute('id', 'editStrikeouts');
+    editPlayerForm.appendChild(strikeoutsLabel);
+    editPlayerForm.appendChild(editStrikeoutsField);
+
+    const sacsLabel = document.createElement('label');
+    sacsLabel.setAttribute('for', 'editSacs');
+    sacsLabel.textContent = 'SF:';
+    const editSacsField = document.createElement('input');
+    editSacsField.setAttribute('type', 'number');
+    editSacsField.setAttribute('value', `${player.numSacs}`);
+    editSacsField.setAttribute('id', 'editSacs');
+    editPlayerForm.appendChild(sacsLabel);
+    editPlayerForm.appendChild(editSacsField);
+
+    const RBILabel = document.createElement('label');
+    RBILabel.setAttribute('for', 'editRBIs');
+    RBILabel.textContent = 'RBI:';
+    const editRBIField = document.createElement('input');
+    editRBIField.setAttribute('type', 'number');
+    editRBIField.setAttribute('value', `${player.numRBIs}`);
+    editRBIField.setAttribute('id', 'editRBIs');
+    editPlayerForm.appendChild(RBILabel);
+    editPlayerForm.appendChild(editRBIField);
+
+    const HBPLabel = document.createElement('label');
+    HBPLabel.setAttribute('for', 'editHBPs');
+    HBPLabel.textContent = 'HBP:';
+    const editHBPField = document.createElement('input');
+    editHBPField.setAttribute('type', 'number');
+    editHBPField.setAttribute('value', `${player.numHBPs}`);
+    editHBPField.setAttribute('id', 'editHBPs');
+    editPlayerForm.appendChild(HBPLabel);
+    editPlayerForm.appendChild(editHBPField);
+
+    // Can add other player stats here
+
+    const submitButton = document.createElement('input');
+    submitButton.setAttribute('type', 'button');
+    submitButton.setAttribute('value', 'Update player');
+    submitButton.addEventListener('click', function() {
+        updatePlayer(player);
+        loadRosterPage();
+    });
+    editPlayerForm.appendChild(submitButton);
+    
+    return editPlayerForm;
+}
+
+// Takes inputs from edit player page and updates player in roster array
+function updatePlayer(player) {
+    player.firstName = document.getElementById('editFirstName').value;
+    player.lastName = document.getElementById('editLastName').value;
+    player.jerseyNum = document.getElementById('editJerseyNum').value;
+    player.numPA = parseInt(document.getElementById('editPA').value);
+    player.numRuns = parseInt(document.getElementById('editRuns').value);
+    player.numSingles = parseInt(document.getElementById('editSingles').value);
+    player.numDoubles = parseInt(document.getElementById('editDoubles').value);
+    player.numTriples = parseInt(document.getElementById('editTriples').value);
+    player.numHRs = parseInt(document.getElementById('editHomeruns').value);
+    player.numWalks = parseInt(document.getElementById('editWalks').value);
+    player.numStrikeouts = parseInt(document.getElementById('editStrikeouts').value);
+    player.numSacs = parseInt(document.getElementById('editSacs').value);
+    player.numRBIs = parseInt(document.getElementById('editRBIs').value);
+    player.numHBPs = parseInt(document.getElementById('editHBPs').value);
+    player.numAtBats = calculateAB(player);
+    player.avg = calculateAVG(player);
+    player.obp = calculateOBP(player);
+
+    saveRoster();
+}
+
+function createReturnButton() {
+    const returnButton = document.createElement('button');
+    returnButton.setAttribute('id', 'returnButton');
+    returnButton.textContent = 'Return';
+    returnButton.addEventListener('click', loadRosterPage);
+    return returnButton;
+}
+
+// Load the edit player page
+function loadEditPlayerPage(player) {
+    const editPlayerDiv = createEditPlayerDiv();
+    const editPlayerForm = createEditPlayerForm(player);
+    editPlayerDiv.appendChild(editPlayerForm);
+    
+    const returnButton = createReturnButton();
+
+    const tabContent = document.getElementById('tabContent');
+    tabContent.textContent = '';
+    tabContent.appendChild(editPlayerDiv);
+    tabContent.appendChild(returnButton);
 }
 
 
+// Load the roster page
 function loadRosterPage() {
 
     const newPlayerFormDiv = createNewPlayerFormDiv();
@@ -400,10 +645,9 @@ function loadRosterPage() {
         loadRoster();
         displayRosterTable();
     } 
-
 }
 
-// local storage functions
+// local storage functions for saving and loading rosters
 function loadRoster() {
     myRoster = JSON.parse(localStorage.getItem("mySavedRoster"));
 }
@@ -411,7 +655,6 @@ function loadRoster() {
 function saveRoster() {
     localStorage.setItem("mySavedRoster", JSON.stringify(myRoster));
 }
-
 
 
 export { loadRosterPage }
